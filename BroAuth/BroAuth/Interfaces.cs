79ac4f061ea39e225dd1901ddaad1ebf63e9517c
Kaelan Fouwels <kfouwels@outlook.com>
2014-03-02 16:26:51 +0000

    Interfaces + initial files

---
+++ BroAuth/BroAuth/Interfaces.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BroAuth
+{
+	public interface ITwitterBro
+	{
+		void Initialise(string username, string password);
+	}
+
+	//public interface IFacebookBro
+	//{
+
+	//}
+}
